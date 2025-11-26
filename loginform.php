14882384edf360cf5c5e53839082b77a5225c87f
Sarah Forde <you@example.com>
2025-11-26 15:11:12 +0000

    Added login html

--- loginform.php
+++ loginform.php
@@ -1,7 +1,25 @@
 <?php
 session_start();
+include("dbcon.php");
+$formUser = $_POST['usr'];
+$formPass = $_POST['pwd'];
 
-$_SESSION['passengerFN']=$_POST['firstname'];
-$_SESSION['passengerSN']=$_POST['surname'];
+$sql = "select username,password from user where username= '$formUser' ";
+$result = mysqli_query($con,$sql);
+
+if ($result->num_rows > 0) { 
+    $row = mysqli_fetch_array ($result);
+if (($formUser==$row['username']) && ($formPass==['password'])) {
+session_start ();
+$_SESSION ['username' ]=$formUser; 
+header ('Location: loggedIn.php');
+}
+else {
+    echo "username and password do not match!";
+}
+}
+else {
+    echo "there are no user matching that name";
+}
 
 ?>
