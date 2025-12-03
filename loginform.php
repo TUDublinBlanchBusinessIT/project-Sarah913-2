87e4c650acb0203b32ddb6a0359bda1f3a12fa69
Sarah Forde <you@example.com>
2025-12-03 15:44:11 +0000

    tiny error fixed

--- loginform.php
+++ loginform.php
@@ -1,14 +1,15 @@
 <?php
 session_start();
 include("dbcon.php");
+
 $formUser = $_POST['usr']?? null;
 $formPwd = $_POST['pwd']?? null;
 
-$sql = "select username,password from user where username= '$formUser' ";
-$result = mysqli_query($con,$sql);
+$sql = "SELECT username,password from user where username= '$formUser' ";
+$result = mysqli_query($conn,$sql);
 
 if ($result && $result ->num_rows > 0) { 
-    $row = mysqli_fetch_asoc ($result);
+    $row = mysqli_fetch_assoc ($result);
 
 if (($formUser==$row['username']) && ($formPwd==['password'])) {
 $_SESSION ['username' ]=$formUser; 
