4f5ea6a38e8be16f379c987e82c64f1fbe2bb469
Sarah Forde <you@example.com>
2025-12-06 19:57:40 +0000

    retreivng data from DB

--- dbcon.php
+++ dbcon.php
@@ -10,7 +10,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-$sql= "INSER INTO MyTable (firstname,lastname,email) VALUES ('JOHH', 'DOE', 'john@example.com')";
+$sql= "INSERT INTO MyTable (firstname,lastname,email) VALUES ('JOHH', 'DOE', 'john@example.com')";
 
 if (mysqli_query($conn, $sql)){
   echo "New record has been added!";
@@ -18,6 +18,16 @@ if (mysqli_query($conn, $sql)){
   echo "ERROR:" .$sql . "<br>" . mysqli_error($conn);
 }
 
-mysqli_close($conn);
+$sql = "SELECT id, firstname, lastname FROM MyTable";
+$result = mysqli_query($conn,$sql);
 
+if (mysqli_num_rows($result)>0){
+  while ($row = mysqli_fetch_assoc($result)) {
+    echo "id: " . $row["id"]. "-Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  } 
+} else {
+    echo "0 results found";
+  }
+
+mysqli_close($conn);
 ?>
