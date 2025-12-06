5656cc7f8db988cb984239bf7921066215314d28
Sarah Forde <you@example.com>
2025-12-06 19:48:24 +0000

    sql command added

--- dbcon.php
+++ dbcon.php
@@ -5,12 +5,19 @@ $password = "1234";
 $dbname = "project_testing";
 $port = 3306;
 
-// Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname, $port);
 
-// Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
+$sql= "INSER INTO MyTable (firstname,lastname,email) VALUES ('JOHH', 'DOE', 'john@example.com')";
+
+if (mysqli_query($conn, $sql)){
+  echo "New record has been added!";
+} else {
+  echo "ERROR:" .$sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 
 ?>
