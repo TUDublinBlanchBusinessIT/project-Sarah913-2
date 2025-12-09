31fb4162a5789d439fe732a62c3fbfda36b09c28
Sarah Forde <you@example.com>
2025-12-09 20:10:15 +0000

    session timeout added

--- selectionProcess.php
+++ selectionProcess.php
@@ -2,13 +2,29 @@
 session_start();
 include('dbcon.php');
 
+if (!isset($_SESSION['lastAccessed'])) {
+    $_SESSION['lastAccessed']=time();
+}
+
+if ($_SESSION['lastAccessed']<(time()-600)) {
+    session_destroy();
+    echo "Session timed out - too long since it has been used";
+    exit;
+
+}
+
+else {
+    $_SESSION['lastAccessed']=time();
+
+}
+
 $sql = "SELECT username FROM users";
 $result = mysqli_query($conn, $sql);
 
 echo "<TABLE>";
 while($row = mysqli_fetch_assoc($result)) {
     $usr = $row ['username'];
-    echo "<h1>Welcome back</h1> <TR><TD>$usr</TD></TR><br>";
+    echo "<h1>Welcome back " . $_SESSION['username']. "</h1><br>";
     echo "What fish did you catch?";
 
 }
@@ -32,11 +48,14 @@ echo "</TABLE>"
         <OPTION value="1">Tuna</OPTION>
         <OPTION value="1">Prawn</OPTION>
 </SELECT>
+<br><br>
+
+<label>What size fish did you catch?:</label><BR><BR>
+<input class="form-control" id="length" type="range" min="1" max="15" value="50" name="fishlength">
+<br><br>
 <input type="submit" value="Save your choice">
 </form>
 
-<label>What size fish did you catch?:</label><BR>
-<input class="form-control" id="length" type="range" min="1" max="5" value="50" name="fishlength">
 
 
     
